b4d4a5508ef8e6212c77da5545806da7434f8722
Drew Cook <c-drew.cook@charter.com>
2021-10-28 09:05:50 -0600

    kadanes link

--- snippets/kadanesAlgorithm.js
+++ snippets/kadanesAlgorithm.js
@@ -1,4 +1,5 @@
 // Kadane's Algorithm - gets largest subarray sum in a list
+// https://medium.com/@rsinghal757/kadanes-algorithm-dynamic-programming-how-and-why-does-it-work-3fd8849ed73d
 function findMaxSubarraySum(nums) {
 	let max_current = -Infinity
 	let max_global = -Infinity
